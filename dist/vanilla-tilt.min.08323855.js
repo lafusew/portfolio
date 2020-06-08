e5a6fa50b3fd0cbdba32ee2a2bbdbd0d7c8f1112
LaFusew <antoine.oddoz@gmail.com>
2020-06-08 11:16:03 +0200

    fixese

--- dist/vanilla-tilt.min.08323855.js
+++ dist/vanilla-tilt.min.08323855.js
@@ -399,7 +399,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56653" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60039" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
